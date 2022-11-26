27a0d27f23f168be1a8b517d60f957cd46c68dd2
Звягин Максим <MAZvyagin@outlook.com>
2022-11-26 21:20:08 +0700

    Рефакторинг

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,42 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tol_func import Tol
-from tol_func_mod import Tol_with_cost
-from optimization_methods import ralgb5, ralgb5_with_proj
 import graw
 from solver import Solve
 
-
-
-def main3():
-  y_mid = np.array([2.185, 1.475, 1.2075, 0.98, 0.85, 0.7275])
-  y_rad = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]) * 0.5
-
-  y_lb = y_mid - y_rad
-  y_ub = y_mid + y_rad
-  x_mid = np.array([0.1, 2.0, 4.0, 6.0, 8.0, 10.0])
-  x_rad = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]) * 0.1
-
-  x_lb = x_mid - x_rad
-  x_ub = x_mid + x_rad
-  tol1 = Tol(x_lb, x_ub, y_lb, y_ub)
-
-  def calcfg2(x):
-    a, b = x[:len(x) // 2], x[len(x) // 2:]
-    return - tol1.tol_value(a, b), np.concatenate(
-      [- tol1.dTda(a, b), - tol1.dTdb(a, b)])
-  #рисуем 3 графика для трех начальных приближений
-  for i in range(3):
-    plt.subplot(3, 1, i + 1)
-    xr, fr, nit, ncalls, ccode = ralgb5(calcfg2,
-                                                     np.array([2.5, 2.9, 3.9, 2.9, 3.9, 2.9]) + 0.3 * i)  # 2.9, 3.9,
-    a, b = xr[:len(xr) // 2], xr[len(xr) // 2:]
-    print(f"a = {a} \nb = {b} \ntolval = {-fr}")
-    fx = lambda a, b, x: np.exp(-x * b) @ (a)
-    graw.draw_interval(fx, a, b, x_lb, x_ub, y_lb, y_ub)
-    print(fr, nit, ncalls, ccode)
-  # graw_for_two_b(a)
-  plt.show()
 
 def main1():
   """
